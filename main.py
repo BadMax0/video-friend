57366946d0eab75b07461b8699576bea6e57b2bf
iamdanteonline <iamdanteonline@gmail.com>
2022-09-13 13:13:18 -0400

    programmer day

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pydub.playback import play
 
 TMP_FILE_NAME = 'current.mp3'
 INPUT_FILE_NAME = 'text.txt'
+DEFAULT_LANG = 'ru'
 
 
 def show_text(text):
@@ -13,8 +14,12 @@ def show_text(text):
     os.system(command)
 
 
-def say(text):
-    tts = gTTS(text, lang='ru')
+def say(text, separator='/'):
+    lang = DEFAULT_LANG
+    if separator in text:
+        text, lang = text.split(separator)
+    print('language - >', lang)
+    tts = gTTS(text, lang=lang)
     tts.save(TMP_FILE_NAME)
     sound = AudioSegment.from_file(TMP_FILE_NAME)
     play(sound)
